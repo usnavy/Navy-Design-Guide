e394eddef728a64b7143c1da672311171447f65a
matthewconti <37333292+matthewconti@users.noreply.github.com>
2018-05-17 10:03:24 -0400

    Update PDFdownload.js

--- js/PDFdownload.js
+++ js/PDFdownload.js
@@ -1,52 +1,39 @@
-function makePDF() {
+function demoFromHTML() {
+    var pdf = new jsPDF('p', 'pt', 'letter');
+    // source can be HTML-formatted string, or a reference
+    // to an actual DOM element from which the text will be scraped.
+    source = $('#customers')[0];
 
-    var quotes = document.getElementById('downloadPDF');
-
-    html2canvas(quotes, {
-        onrendered: function(canvas) {
-
-        //! MAKE YOUR PDF
-        var pdf = new jsPDF('p', 'pt', 'letter');
-
-        for (var i = 0; i <= quotes.clientHeight/980; i++) {
-            //! This is all just html2canvas stuff
-            var srcImg  = canvas;
-            var sX      = 820;
-            var sY      = 980*i; // start 980 pixels down for every new page
-            var sWidth  = 900;
-            var sHeight = 980;
-            var dX      = 0;
-            var dY      = 0;
-            var dWidth  = 900;
-            var dHeight = 980;
-
-            window.onePageCanvas = document.createElement("canvas");
-            onePageCanvas.setAttribute('width', 900);
-            onePageCanvas.setAttribute('height', 980);
-            var ctx = onePageCanvas.getContext('2d');
-            // details on this usage of this function: 
-            // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images#Slicing
-            ctx.drawImage(srcImg,sX,sY,sWidth,sHeight,dX,dY,dWidth,dHeight);
-
-            // document.body.appendChild(canvas);
-            var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
-
-            var width         = onePageCanvas.width;
-            var height        = onePageCanvas.clientHeight;
-
-            //! If we're on anything other than the first page,
-            // add another page
-            if (i > 0) {
-                pdf.addPage(612, 791); //8.5" x 11" in pts (in*72)
-            }
-            //! now we declare that we're working on that page
-            pdf.setPage(i+1);
-            //! now we add content to that page!
-            pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width*.62), (height*.62));
-
+    // we support special element handlers. Register them with jQuery-style 
+    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
+    // There is no support for any other type of selectors 
+    // (class, of compound) at this time.
+    specialElementHandlers = {
+        // element with id of "bypass" - jQuery style selector
+        '#bypassme': function (element, renderer) {
+            // true = "handled elsewhere, bypass text extraction"
+            return true
         }
-        //! after the for loop is finished running, we save the pdf.
-        pdf.save('test.pdf');
-    }
-  });
+    };
+    margins = {
+        top: 80,
+        bottom: 60,
+        left: 40,
+        width: 522
+    };
+    // all coords and widths are in jsPDF instance's declared units
+    // 'inches' in this case
+    pdf.fromHTML(
+    source, // HTML string or DOM elem ref.
+    margins.left, // x coord
+    margins.top, { // y coord
+        'width': margins.width, // max width of content on PDF
+        'elementHandlers': specialElementHandlers
+    },
+
+    function (dispose) {
+        // dispose: object with X, Y of the last line add to the PDF 
+        //          this allow the insertion of new lines after html
+        pdf.save('Test.pdf');
+    }, margins);
 }
